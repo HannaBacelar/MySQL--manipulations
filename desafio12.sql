88eaa0e573fc635e18a3915caf264cb8061ecb88
Hanna Bacelar <hannabacelar@hotmail.com>
2022-05-25 16:06:03 -0300

    :lipstick: feat: mostrando as submitted_date de purchase_orders em que a submitted_date é do dia 26 de abril de 2006.

---
+++ desafio12.sql
@@ -0,0 +1,2 @@
+SELECT submitted_date from northwind2.purchase_orders
+WHERE 	DATE(submitted_date) = '2006-04-26';
---
+++ desafio13.sql
@@ -0,0 +1,2 @@
+SELECT supplier_id FROM northwind2.purchase_orders
+where supplier_id = 1 or 2;
